0a302cbbba2a7676ad48da227e765bd6845e3f8c
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2025-03-27 23:37:05 +0700

    feat: update project structure

---
+++ src/styles/fonts.ts
@@ -0,0 +1,12 @@
+import { Poppins, Nunito } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+});
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export { poppins, nunito };
