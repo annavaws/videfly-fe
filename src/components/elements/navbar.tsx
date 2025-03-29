fadbf7bc19ab6d37b3318d3b8f4c50915b6f7e7d
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2025-03-29 22:48:43 +0700

    style: responsiveness for navbar

--- src/components/elements/navbar.tsx
+++ src/components/elements/navbar.tsx
@@ -61,17 +61,20 @@ const Navbar = () => {
         </div>
       )}
 
-      <div className="flex lg:w-2/6 justify-center space-x-4">
-        <button className="border border-gray-300 px-4 py-2 bg-gray-100 text-black rounded-lg hover:scale-105 duration-150 transition-all ease-in-out cursor-pointer">
+      <div className="hidden lg:flex lg:w-2/6 justify-center space-x-4">
+        <button className="w-border border-gray-300 px-4 py-2 bg-gray-100 text-black rounded-lg hover:scale-105 duration-150 transition-all ease-in-out cursor-pointer">
           Masuk
         </button>
-        <button className="hidden lg:block px-4 py-2 bg-gradient-to-bl from-[#7F52FF] to-[#FB8570] text-white rounded-lg hover:scale-105 transform duration-150 transition-all ease-in-out cursor-pointer">
+        <button className="px-4 py-2 bg-gradient-to-bl from-[#7F52FF] to-[#FB8570] text-white rounded-lg hover:scale-105 transform duration-150 transition-all ease-in-out cursor-pointer">
           Buat Video
         </button>
       </div>
 
       {/* hamburger icon (when screen is equal or smaller than md*/}
-      <div className="lg:hidden flex items-center justify-end">
+      <div className="lg:hidden flex items-center justify-end w-full gap-4">
+        <button className="w-border border-gray-300 px-4 py-2 bg-gray-100 text-black rounded-lg hover:scale-105 duration-150 transition-all ease-in-out cursor-pointer">
+          Masuk
+        </button>
         <button className="text-2xl" onClick={() => setMenuOpen(!menuOpen)}>
           &#9776;
         </button>
